868686438e3fce1bc2830bfef418052813b53466
beck <beckhan@busiui.com>
2020-03-20 09:57:54 +0800

    initial vue project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+	<custom-square></custom-square>
+	
+	<button v-on:click="busiUiClick()">Start BusiUi</button>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  methods:{
+	  busiUiClick:function(){
+		  alert('123');
+	  },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
